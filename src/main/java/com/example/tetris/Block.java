5d57f28c3fa08a08837768a142f23a4052d00ec1
Luuk Bouma <luuk.bouma@student.hu.nl>
2023-05-12 15:36:35 +0200

    pieces

---
+++ src/main/java/com/example/tetris/Block.java
@@ -0,0 +1,12 @@
+package com.example.tetris;
+
+import javafx.scene.paint.Color;
+
+public class Block {
+    private final int size = 40;
+    private final Color colour;
+
+    public Block(Color colour) {
+        this.colour = colour;
+    }
+}
