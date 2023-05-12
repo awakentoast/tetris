4f5c1774af2665ee665182773dc46ade9f375614
Luuk Bouma <luuk.bouma@student.hu.nl>
2023-05-12 15:56:20 +0200

    lol

--- src/main/java/com/example/tetris/Piece.java
+++ src/main/java/com/example/tetris/Piece.java
@@ -1,4 +1,11 @@
 package com.example.tetris;
 
+import javafx.scene.paint.Color;
+
 public class Piece {
+
+    private final int[][] blockShapes = new int[4][4];
+    private final Color colour;
+
+    
 }
